eee75e230ac58ddabaae7845860d720dd00e9d37
Ahmad <aksoftware98@hotmail.com>
2021-05-19 02:04:27 +0300

    Implement the AK Toolbar

---
+++ src/AK Timetable/Components/AKCommand.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AK_Timetable.Components
+{
+    public partial class AKCommand : ComponentBase
+    {
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+
+        [Parameter]
+        public string Icon { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        private void OnButtonClick()
+        {
+            if (!IsDisabled)
+                OnClick.InvokeAsync();
+        }
+    }
+}
